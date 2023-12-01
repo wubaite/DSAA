95fa76707b9264e2d23b8b1d343f93ce99154780
boteWu <526380248@qq.com>
2023-12-01 13:35:35 +0800

    Update PriorityQueue.js
    fix：修改一些bug

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -2,7 +2,7 @@
  * @Author: Tiga 526380248@qq.com
  * @Date: 2023-12-01 08:58:59
  * @LastEditors: Tiga 526380248@qq.com
- * @LastEditTime: 2023-12-01 09:59:59
+ * @LastEditTime: 2023-12-01 13:34:42
  * @FilePath: /DSAA/PriorityQueue.js
  * @Description: 基于数组实现优先级队列
  */
@@ -18,7 +18,7 @@
 
 function PriorityQueue() {
 
-  this.item = [];
+  this.items = [];
 
   function Element(element, priority) {
     // 数据
@@ -30,43 +30,42 @@ function PriorityQueue() {
   PriorityQueue.prototype.unqueue = function (element, priority) {
     let ele = new Element(element, priority);
 
-    if (this.item.length === 0) {
-      // 队列中没有值
-      this.item.push(ele);
+    if (this.items.length === 0) {
+      // 队列中没有数据
+      this.items.push(ele);
     } else {
       // 找出对应的优先级位置，添加到对应位置
-      for (let i = 0; i < this.item.length; i++) {
-        if (ele.priority < this.item[i].priority) {
-          this.item.splice(i, 0, ele);
+      for (let i = 0; i < this.items.length; i++) {
+        if (ele.priority < this.items[i].priority) {
+          this.items.splice(i, 0, ele);
           return;
         }
       }
       // 如果其优先级为最大，直接添加到队列末尾
-      this.item.push(ele);
+      this.items.push(ele);
     }
-
   }
 
-  Queue.prototype.dequeue = function () {
+  PriorityQueue.prototype.dequeue = function () {
     return this.items.shift();
   }
 
-  Queue.prototype.front = function () {
+  PriorityQueue.prototype.front = function () {
     return this.items[0];
   }
 
-  Queue.prototype.isEmpty = function () {
+  PriorityQueue.prototype.isEmpty = function () {
     return this.items.length === 0;
   }
 
-  Queue.prototype.size = function () {
+  PriorityQueue.prototype.size = function () {
     return this.items.length;
   }
 
-  Queue.prototype.toString = function () {
+  PriorityQueue.prototype.toString = function () {
     let result = "";
     for (let i = 0; i < this.items.length; i++) {
-      result += this.items[i] + " ";
+      result += `优先级：${this.items[i].priority} - 数据：${this.items[i].element} \n`;
     }
     return result;
   }
@@ -83,6 +82,6 @@ pq.unqueue("3GS", 1);
 
 
 console.log('====================================');
-console.log(pq.item);
+console.log(pq.toString());
 console.log('====================================');
 
